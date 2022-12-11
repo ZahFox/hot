04c81425df17a39316669e2f4b9a58984708c725
ZahFox <ZahFoxTales@gmail.com>
2022-12-11 06:07:50 +0000

    chore: remove initial sample commands

--- src/Hot.Cli/Program.cs
+++ src/Hot.Cli/Program.cs
@@ -1,5 +1,4 @@
-﻿using Bullseye;
-using System.CommandLine;
+﻿using System.CommandLine;
 using Hot.Core;
 using Hot.Core.IO;
 using Hot.Core.Exe;
@@ -25,8 +24,6 @@ static void ConfigureTools()
 static Command ConfigureCli()
 {
   var rootCommand = new RootCommand();
-  var helloCommand = new Command("hello", "Prints 'Hello, World!'");
-  var tasksCommand = new Command("tasks", "Executes a graph of async tasks.");
   var exeCommand = new Command("exe", "Run an executable file.");
   var toolCommand = new Command("tool", "Use one of the common system tools.");
 
@@ -60,31 +57,6 @@ static Command ConfigureCli()
   )
   { Arity = ArgumentArity.ZeroOrMore };
 
-  async Task RunHelloCommand(string msg = "World")
-  {
-    HelloWorld.Print(msg);
-    await Task.Delay(2000);
-    HelloWorld.Print(msg);
-  }
-
-  async Task RunTasksCommand()
-  {
-    var targets = new Targets();
-
-    targets.Add(
-        "print1",
-        async () => await System.Console.Out.WriteLineAsync("111")
-    );
-
-    targets.Add(
-        "print2",
-        async () => await System.Console.Out.WriteLineAsync("222")
-    );
-
-    targets.Add("default", dependsOn: new[] { "print1", "print2" });
-    await targets.RunWithoutExitingAsync(new[] { "default" }, outputWriter: new VoidTextWriter());
-  }
-
   async Task RunExeCommand(string name, List<string> args)
   {
     var exe = new Exe { Name = name, Args = args.AsReadOnly() };
@@ -114,19 +86,14 @@ static Command ConfigureCli()
     }
   }
 
-  rootCommand.Add(helloCommand);
-  rootCommand.Add(tasksCommand);
   rootCommand.Add(exeCommand);
   rootCommand.Add(toolCommand);
 
-  helloCommand.Add(msgOption);
   exeCommand.Add(exeNameArg);
   exeCommand.Add(exeArgsArg);
   toolCommand.Add(toolNameArg);
   toolCommand.Add(toolArgsArg);
 
-  helloCommand.SetHandler(RunHelloCommand, msgOption);
-  tasksCommand.SetHandler(RunTasksCommand);
   exeCommand.SetHandler(RunExeCommand, exeNameArg, exeArgsArg);
   toolCommand.SetHandler(RunToolCommand, toolNameArg, toolArgsArg);
 
